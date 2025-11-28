455871b6f6615f9e51669f9a3dd38fd7ecb2fdd3
Matt <mattlocktom@gmail.com>
2025-11-28 19:02:08 +1300

    feat(profile): WIP - Profile page implementation

--- client/apis/users.ts
+++ client/apis/users.ts
@@ -2,7 +2,8 @@ import request from 'superagent'
 import { User, UserData } from '../../models/user'
 import { Post } from '../../models/post'
 
-const rootURL = new URL(`/api/v1`, document.baseURI)
+// const rootURL = new URL(`/api/v1`, document.baseURI)  ||| updated to new code in ‹feature/profile-page› branch
+const rootURL = `/api/v1`
 
 // ARCHITECTURAL NOTE: All user-facing API calls in this project should use
 // the string `authId` for identification, not the internal numeric database `id`.
