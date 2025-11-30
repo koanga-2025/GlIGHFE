c086326d82d0642e18f14739503af867d5ada89a
James Hooson <jmhooson@hotmail.com>
2025-12-01 11:34:11 +1300

    feat:onboarding complete, added pronounciation changer, onboarding slides

---
+++ client/components/Embla.tsx
@@ -0,0 +1,62 @@
+import useEmblaCarousel from 'embla-carousel-react'
+import type { EmblaOptionsType } from 'embla-carousel'
+import { useCallback, useEffect, useState } from 'react'
+
+type PropType = {
+  textBoxes: string[]
+  options?: EmblaOptionsType
+}
+
+const EmblaCarousel = (props: PropType) => {
+  const { textBoxes = [], options } = props // Added default
+  const [emblaRef, emblaApi] = useEmblaCarousel(options)
+  const [currentIndex, setCurrentIndex] = useState(0)
+
+  const onSelect = useCallback(() => {
+    if (!emblaApi) return
+    setCurrentIndex(emblaApi.selectedScrollSnap())
+  }, [emblaApi])
+
+  useEffect(() => {
+    if (!emblaApi) return
+    onSelect()
+    emblaApi.on('select', onSelect)
+    return () => {
+      emblaApi.off('select', onSelect)
+    }
+  }, [emblaApi, onSelect])
+
+  const isLastSlide = currentIndex === textBoxes.length - 1
+
+  return (
+    <section className="w-full overflow-hidden">
+      <div className="w-full overflow-hidden" ref={emblaRef}>
+        <div className="flex">
+          {textBoxes.map((text, index) => (
+            <div
+              className="h-60% flex min-w-0 flex-[0_0_100%] items-center justify-center p-8"
+              key={index}
+            >
+              <div className="max-w-2xl text-center text-3xl">
+                <p>{text}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+      {!isLastSlide && <p className="text-5xl">➽➽➽</p>}
+      {isLastSlide && (
+        <button
+          onClick={() => {
+            window.location.href = '/feed'
+          }}
+          className="hover:bg-success-strong focus:ring-success-medium shadow-xs text-md ml-7 box-border rounded-xl border-2 border-black bg-lime-400 p-0.5 px-4 py-2.5 font-bold leading-5 focus:outline-none focus:ring-4"
+        >
+          I AGREE I WILL USE NO TEXT
+        </button>
+      )}
+    </section>
+  )
+}
+
+export default EmblaCarousel
--- client/components/Onboarding.tsx
+++ client/components/Onboarding.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react'
+import EmblaCarousel from './Embla'
 
 function Onboarding() {
   const glyphArray: string[] = [
@@ -7,10 +8,11 @@ function Onboarding() {
     'Glig-hefe',
     'Glyphee-ee',
     'Gli-ephee',
-    'Gly-phae'
+    'Gly-phae',
   ]
-  const [ Glyph, setGlyph ] = useState(glyphArray)
+  const [Glyph, setGlyph] = useState(glyphArray)
   // const Glyph: string = 'Glyph-ee' // make setState to change through different pronounciations!!!
+  const currentGlyph = Glyph[random(0, Glyph.length - 1)]
 
   useEffect(() => {
     // Lock scrolling when component mounts
@@ -22,39 +24,25 @@ function Onboarding() {
   }, [])
 
   return (
-    <div className="font-body relative flex h-screen min-h-screen flex-col items-center overflow-hidden bg-gradient-to-b from-green-200 to-green-500 p-4 text-center">
-      <h1 className="mb-5 text-xl font-bold underline">What is GlIFGHE</h1>
-      <p className="rounded-xl border-2 border-black">
-        GlIFGHE (pronounced {`${Glyph}`}) is a social media platform that communicates
-        through pure visual signal: icons, symbols, and images designed for
-        universal interpretation—no language required.
-      </p>
-      <br />
-      <p className="rounded-xl border-2 border-black">
-        Within this system, text does not exist. No words, No captions, No
-        written markers of any kind.
-      </p>
-      <br />
-      <p className="rounded-xl border-2 border-black">
-        GlIFGHE leverages visual communication to drive authentic user
-        engagement. Zero text, zero barriers. See with eyes. Think with heart.
-        Your journey begins.
-      </p>
-      <br />
-      <p className="rounded-xl border-2 border-black font-bold">
-        THAR BE NO WORDS NOR TEXT BEYOND THIS POINT, ON ME HONOR!
-      </p>
-      <br />
-      <button
-        onClick={() => {
-          window.location.href = '/feed'
-        }}
-        className="hover:bg-success-strong focus:ring-success-medium shadow-xs ml-7 box-border rounded-xl border-2 border-black bg-lime-400 p-0.5 px-4 py-2.5 text-sm font-medium leading-5 focus:outline-none focus:ring-4"
-      >
-        I AGREE I WILL USE NO TEXT
-      </button>
-      <footer className="right-0 p-4 absolute bottom-0 left-0 text-white"></footer>
+    <div className="relative flex h-screen min-h-screen flex-col items-center overflow-hidden bg-gradient-to-b from-green-200 to-green-500 p-4 text-center font-body">
+      <h1 className="mb-5 text-5xl font-bold underline">What is GlIFGHE</h1>
+
+      <EmblaCarousel
+        textBoxes={[
+          `GlIFGHE (pronounced ${currentGlyph}) is a social media platform that communicates through pure visual signal: icons, symbols, 
+          and images designed for universal interpretation—no language required.`,
+          'Within this system, text does not exist. No words, No captions, No written markers of any kind.',
+          'GlIFGHE leverages visual communication to drive authentic user engagement. Zero text, zero barriers. See with eyes. Think with heart.Your journey begins.',
+          'THAR BE NO WORDS NOR TEXT BEYOND THIS POINT, ON ME HONOR!',
+        ]}
+      />
+      <footer className="absolute bottom-0 left-0 right-0 p-4 text-white"></footer>
     </div>
   )
 }
 export default Onboarding
+
+function random(min: number, max: number): number {
+  const num = Math.floor(Math.random() * (max - min + 1)) + min
+  return num
+}
